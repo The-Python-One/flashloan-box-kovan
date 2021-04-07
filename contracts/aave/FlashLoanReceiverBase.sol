5824741267f40c5588c82adb1e7f7fa58f432da9
The-Python-One <59634806+The-Python-One@users.noreply.github.com>
2021-04-08 04:58:38 +0800

    Update FlashLoanReceiverBase.sol

--- contracts/aave/FlashLoanReceiverBase.sol
+++ contracts/aave/FlashLoanReceiverBase.sol
@@ -2,14 +2,14 @@ pragma solidity ^0.6.6;
 
 import "@openzeppelin/contracts/math/SafeMath.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
-import "@openzeppelin/contracts/token/ERC20/SafeERC20.sol";
+//import "@openzeppelin/contracts/token/ERC20/SafeERC20.sol";
 import "./IFlashLoanReceiver.sol";
 import "./ILendingPoolAddressesProvider.sol";
 import "../utils/Withdrawable.sol";
 
 abstract contract FlashLoanReceiverBase is IFlashLoanReceiver, Withdrawable {
 
-    using SafeERC20 for IERC20;
+    //using SafeERC20 for IERC20;
     using SafeMath for uint256;
 
     address constant ethAddress = 0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE;
